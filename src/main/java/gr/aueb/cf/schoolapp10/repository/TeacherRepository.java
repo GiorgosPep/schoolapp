70bb427940d92d9c6eb62133672c44bdf032d677
gpeppas <gpeppas@ajpsoft.gr>
2026-08-13 16:04:47 +0300

    feat: add methods for retrieving non-deleted Teachers in TeacherRepository

--- src/main/java/gr/aueb/cf/schoolapp10/repository/TeacherRepository.java
+++ src/main/java/gr/aueb/cf/schoolapp10/repository/TeacherRepository.java
@@ -1,6 +1,10 @@
 package gr.aueb.cf.schoolapp10.repository;
 
 import gr.aueb.cf.schoolapp10.model.Teacher;
+import jakarta.persistence.Entity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -11,4 +15,9 @@ public interface TeacherRepository extends JpaRepository<Teacher, Long> {
     Optional<Teacher> findByVat(String vat);
     Optional<Teacher> findByUuid(UUID uuid);
 
+    @EntityGraph(attributePaths = {"region"})
+    Page<Teacher> findAllByDeletedFalse(Pageable pageable);
+
+    Optional<Teacher> findByVatDeletedFalse(String vat);
+    Optional<Teacher> findByUuidAndDeletedFalse(UUID uuid);
 }
