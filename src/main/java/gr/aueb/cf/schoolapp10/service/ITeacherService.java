29fe02b1b2941b22820e11310c07cc392aca720a
gpeppas <gpeppas@ajpsoft.gr>
2026-08-13 16:17:11 +0300

    feat: add pagination methods for retrieving Teachers in TeacherService

--- src/main/java/gr/aueb/cf/schoolapp10/service/ITeacherService.java
+++ src/main/java/gr/aueb/cf/schoolapp10/service/ITeacherService.java
@@ -4,6 +4,8 @@ import gr.aueb.cf.schoolapp10.core.exceptions.EntityAlreadyExistsException;
 import gr.aueb.cf.schoolapp10.core.exceptions.EntityInvalidArgumentException;
 import gr.aueb.cf.schoolapp10.dto.TeacherInsertDTO;
 import gr.aueb.cf.schoolapp10.dto.TeacherReadOnlyDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ITeacherService {
 
@@ -11,4 +13,7 @@ public interface ITeacherService {
         throws EntityAlreadyExistsException, EntityInvalidArgumentException;
 
     boolean isTeacherExistsByVat(String vat);
-}
+    Page<TeacherReadOnlyDTO> getPaginatedTeachersDeletedFalse(Pageable pageable);
+    Page<TeacherReadOnlyDTO> getPaginatedTeachers(Pageable pageable);
+
+    }
