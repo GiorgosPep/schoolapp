2fdb42856f38f41f10d65d1bb0867f6b6b9dab7c
gpeppas <gpeppas@ajpsoft.gr>
2026-08-16 16:36:14 +0300

    feat: add updateTeacher method to ITeacherService for editing teacher details

--- src/main/java/gr/aueb/cf/schoolapp10/service/ITeacherService.java
+++ src/main/java/gr/aueb/cf/schoolapp10/service/ITeacherService.java
@@ -2,6 +2,8 @@ package gr.aueb.cf.schoolapp10.service;
 
 import gr.aueb.cf.schoolapp10.core.exceptions.EntityAlreadyExistsException;
 import gr.aueb.cf.schoolapp10.core.exceptions.EntityInvalidArgumentException;
+import gr.aueb.cf.schoolapp10.core.exceptions.EntityNotFoundException;
+import gr.aueb.cf.schoolapp10.dto.TeacherEditDTO;
 import gr.aueb.cf.schoolapp10.dto.TeacherInsertDTO;
 import gr.aueb.cf.schoolapp10.dto.TeacherReadOnlyDTO;
 import org.springframework.data.domain.Page;
@@ -12,8 +14,13 @@ public interface ITeacherService {
     TeacherReadOnlyDTO saveTeacher(TeacherInsertDTO teacherInsertDTO)
         throws EntityAlreadyExistsException, EntityInvalidArgumentException;
 
+    TeacherReadOnlyDTO updateTeacher(TeacherEditDTO teacherEditDTO)
+        throws EntityNotFoundException, EntityAlreadyExistsException, EntityInvalidArgumentException;
+
     boolean isTeacherExistsByVat(String vat);
     Page<TeacherReadOnlyDTO> getPaginatedTeachersDeletedFalse(Pageable pageable);
     Page<TeacherReadOnlyDTO> getPaginatedTeachers(Pageable pageable);
+
+
 
     }
